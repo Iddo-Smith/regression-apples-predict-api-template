d408b94198ca3005d0f988c8c5d07a677b746765
Iddo-Smith <iddo.smith@icloud.com>
2021-05-26 10:46:04 +0200

    Update request.py

--- utils/request.py
+++ utils/request.py
@@ -14,7 +14,7 @@
     Description: This file contains code used to formulate a POST request
     which can be used to develop/debug the Model API once it has been
     deployed.
-
+    #url = 'http://127.0.0.1:5000/api_v0.1'
 """
 
 # Import dependencies
@@ -36,7 +36,7 @@ feature_vector_json = test.iloc[1].to_json()
 # replace the URL below with its public IP:
 
 url = 'http://34.245.158.7:5000/api_v0.1'
-#url = 'http://127.0.0.1:5000/api_v0.1'
+
 
 # Perform the POST request.
 print(f"Sending POST request to web server API at: {url}")
